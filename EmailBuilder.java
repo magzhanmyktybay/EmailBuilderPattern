24859394f7640d84a077e5a7eab0af32239a27fe
magzhanmyktybay <magzhanmyktybay@gmail.com>
2026-09-10 19:43:50 +0500

    EmailBuilder

---
+++ EmailBuilder.java
@@ -0,0 +1,6 @@
+public interface EmailBuilder {
+    EmailBuilder setFrom(String from);
+    EmailBuilder setTo(String to);
+    EmailBuilder setSubject(String subject);
+    EmailBuilder setBody(String body);
+}
